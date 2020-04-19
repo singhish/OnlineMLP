8d2cd05b346e7a2bdb75f5958f4a294cf7424574
Ishrat <ishrat@login001.cm.cluster>
2020-04-18 23:49:51 -0400

    updated scripts

--- offline-bench/sweep.sh
+++ offline-bench/sweep.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=offlinemlp
-#SBATCH -n 28
+#SBATCH -n 100
 #SBATCH -o py_out%j.out
 #SBATCH -e py_err%j.err
 
-echo 'History Length,Forecast Length,Units,Epochs,Loss'
+echo 'History Length,Forecast Length,Units,Epochs,Loss' >> offline-results.csv
 
 # forecast length
 for f in {10..100..10}; do
@@ -14,7 +14,7 @@ for f in {10..100..10}; do
     for u in {20..200..20}; do
       # epochs
       for e in {1..10}; do
-	(( i=i%28 )); (( i++==0 )) && wait
+	(( i=i%100 )); (( i++==0 )) && wait
         srun -n 1 -c 1 --exclusive run.sh -f $f -l $l -e $e -u $u >> offline-results.csv &
       done
     done
