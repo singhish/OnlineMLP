da6b952b444d4975adcabab32a85738427116f4f
Ish <ishrat@email.sc.edu>
2020-05-28 18:46:48 -0400

    Fix submit.sh

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
-echo 'History Length,Forecast Length,Units,Epochs,Interval,Loss' >> results.csv
+echo 'Sinusoids,Standard Deviations,History Length,Forecast Length,Units,Epochs,Interval,Loss' >> results.csv
 
 for s in 1 2 3; do
   for std in 1 5 10; do
